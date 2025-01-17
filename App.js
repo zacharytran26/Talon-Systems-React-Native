aae953523723e34a94d97ffe869e009400e7c643
zacharytran26 <122825561+zacharytran26@users.noreply.github.com>
2025-01-17 11:27:33 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
-import React, { useState, useContext} from 'react';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Router } from './routes/Route';
-import { useAuth, AuthProvider } from './screens/ThemeContext';
-
+import React, { useState, useContext } from "react";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import { Router } from "./routes/Route";
+import { useAuth, AuthProvider } from "./screens/ThemeContext";
 
 const App = () => (
   <>
-  <ApplicationProvider {...eva} theme = {eva.light}>
-
-    <IconRegistry icons={EvaIconsPack} />
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
-
-  </ApplicationProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <IconRegistry icons={EvaIconsPack} />
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+    </ApplicationProvider>
   </>
 );
 
